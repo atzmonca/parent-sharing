4ed806da57ac6b72c93b7721d4638d5e2d7338c5
unknown <atzmon.carbi@gmail.com>
2018-08-04 15:08:17 +0300

    redux final version section 7

--- src/features/testarea/testReducer.jsx
+++ src/features/testarea/testReducer.jsx
@@ -1,10 +1,18 @@
 import { INCREMENT_COUNTER, DECREMENT_COUNTER } from "./testConstants";
-
+import { createReducer } from '../../app/common/util/reducerUtil'
 const initialState = {
   data: 4225
 };
 
-const testReducer = (state = initialState, action) => {
+export const incrementCounter = (state,payload)=>{
+  return { ...state, data: state.data + 1 };
+}
+
+export const decrementCounter = (state,payload)=>{
+  return { ...state, data: state.data - 1 };
+}
+
+/* const testReducer = (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT_COUNTER:
       return { ...state, data: state.data + 1 };
@@ -14,5 +22,8 @@ const testReducer = (state = initialState, action) => {
       return state;
   }
 
-};
-export default testReducer;
+}; */
+export default createReducer(initialState,{
+[INCREMENT_COUNTER]: incrementCounter,
+[DECREMENT_COUNTER]: decrementCounter
+});
